ca11afa4daf0c131fc95c36f6c1d2331a3226953
Pieter Claerhout <pieter@yellowduck.be>
2019-12-19 08:07:13 +0100

    Fixed a race condition

--- cmd/example-command-output/main.go
+++ cmd/example-command-output/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"os/exec"
 
 	"github.com/pieterclaerhout/go-log"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -23,28 +21,36 @@ func main() {
 	// Use the same pipe for standard error
 	cmd.Stderr = cmd.Stdout
 
+	// Make a new channel which will be used to ensure we get all output
+	done := make(chan struct{})
+
 	// Create a scanner which scans r in a line-by-line fashion
 	scanner := bufio.NewScanner(r)
+
+	// Use the scanner to scan the output line by line and log it
+	// It's running in a goroutine so that it doesn't block
+	go func() {
+
+		// Read line by line and process it
+		for scanner.Scan() {
+			line := scanner.Text()
+			log.Info(line)
+		}
+
+		// We're all done, unblock the channel
+		done <- struct{}{}
+
+	}()
 
 	// Start the command and check for errors
 	err := cmd.Start()
 	log.CheckError(err)
 
-	// Start a new error group, run the command and wait for it to finish
-	// We are running this in a go routine so that we don't block
-	errs, _ := errgroup.WithContext(context.Background())
-	errs.Go(func() error {
-		return cmd.Wait()
-	})
+	// Wait for all output to be processed
+	<-done
 
-	// Use the scanner to scan the output line by line and log ti
-	for scanner.Scan() {
-		line := scanner.Text()
-		log.Info(line)
-	}
-
-	// Wait for the command to finish and check for errors
-	err = errs.Wait()
+	// Wait for the command to finish
+	err = cmd.Wait()
 	log.CheckError(err)
 
 }
